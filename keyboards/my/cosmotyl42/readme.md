d0d3a52ef603104f0d836d8434c9a7386228373c
logico-philosophical <50512456+logico-philosophical@users.noreply.github.com>
2024-09-14 11:41:43 +0900

    Add my keyboards

---
+++ keyboards/my/cosmotyl42/readme.md
@@ -0,0 +1,6 @@
+# my/cosmotyl42
+
+```bash
+make cosmotyl42:default
+make cosmotyl42:vial
+```
---
+++ keyboards/my/piantor/keymaps/vial/config.h
@@ -0,0 +1,14 @@
+/* keyboard uid */
+#define VIAL_KEYBOARD_UID {0xDC, 0x6D, 0x9C, 0x42, 0x53, 0x42, 0x75, 0x0F}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
+
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM 0
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
